ea048592cded1505e580566ced0e060a32d02eaa
Devin Rasmussen <devin.rasmussen@icloud.com>
2022-04-17 11:57:15 -0500

    provide correct links and helpful text for knowing what to do on initial screen

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -5,12 +5,14 @@ import indexStyles from "~/styles/index.css";
 export const links = () => {
   return [{ rel: "stylesheet", href: indexStyles }];
 };
-
 export default function Index() {
   return (
-    <div className="button-container">
-      <Button href="/">Restaurant</Button>
-      <Button href="/">Dish</Button>
+    <div className="centered-content-container">
+      What are you looking for?
+      <div className="button-container">
+        <Button href="/restaurants">Restaurant</Button>
+        <Button href="/dishes">Dish</Button>
+      </div>
     </div>
   );
 }
