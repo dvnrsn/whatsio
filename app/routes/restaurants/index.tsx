4a9efa9f93dff569d72a10ee63dfb6f30f6860fc
Devin Rasmussen <devin.rasmussen@icloud.com>
2022-04-25 14:32:12 -0500

    Implement google maps api on restaurant page. Show picture

--- app/routes/restaurants/index.tsx
+++ app/routes/restaurants/index.tsx
@@ -1,10 +1,22 @@
+import { LinksFunction } from "@remix-run/node";
+import { useState } from "react";
 import Input from "~/components/input";
+import useGoogleApi from "~/hooks/useGoogleApi";
+import restaurantStyles from "~/styles/restaurants.css";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: restaurantStyles }];
+};
 
 export default function () {
+  const image = useGoogleApi();
+  const [imageLoaded, setImageLoaded] = useState(false);
   return (
     <div className="centered-content-container">
       Which Restauraunt?
-      <Input autoFocus type="text" />
+      <Input id="google-auto-complete" autoFocus type="text" />
+      {image && <img className="restaurant-image" src={image} />}
+      {image && !imageLoaded && <div className="image-placeholder" />}
     </div>
   );
 }
