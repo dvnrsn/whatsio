b20be9868582aa896769342f52c1c159a3dcef42
Devin Rasmussen <devin.rasmussen@icloud.com>
2022-04-27 18:08:57 -0500

    add loader and route to display it

---
+++ app/components/loader.tsx
@@ -0,0 +1,14 @@
+import { motion } from "framer-motion";
+export default function Loader() {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      className="whatsio-loader-container"
+    >
+      <div className="whatsio-loader" />
+      <div className="whatsio-loader whatsio-loader-second" />
+    </motion.div>
+  );
+}
---
+++ app/routes/loader.tsx
@@ -0,0 +1,9 @@
+import Loader from "~/components/loader";
+
+export default function loader() {
+  return (
+    <div className="centered-content-container">
+      <Loader />
+    </div>
+  );
+}
